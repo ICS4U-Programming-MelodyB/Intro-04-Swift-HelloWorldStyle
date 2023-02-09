39c439c6d78b8d3b30202f26a3d85805204dfb35
Melody Berhane <melody.berhane@stu.ocsb.ca>
2023-02-09 19:07:08 +0000

    Commit message

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,10 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Melody Berhane
+//  Created on 2023-2-8
+//  Version 1.0
+//  Copyright (c) 2023 Melody Berhane. All rights reserved.
+//
+//  Prints Hello, World with Style!.
+print("Hello, World with Style!")
